2e7f4e3f01417b00661ee9d1e2ada358beb99115
Adam Akbar <addampz@gmail.com>
2024-12-01 21:36:06 +0800

    Add doc block, remove `return` keyword, and sync the jsx-tsx code (#1)

--- components/react-ts/button.tsx
+++ components/react-ts/button.tsx
@@ -1,18 +1,19 @@
 'use client'
 
-import React from 'react'
+import React, { type ButtonHTMLAttributes, forwardRef } from 'react'
 
-export type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>
+export type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>
 
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(({ children, className, ...props }) => {
-    return (
-        <button
-            className={`text-sm font-semibold shadow-[3px_3px_0_0_#000] border-2 border-black bg-sky-300 h-10 px-4 rounded hover:translate-x-[3px] hover:shadow-none hover:translate-y-[3px] transition-all ${className}`}
-            {...props}
-        >
-            {children}
-        </button>
-    )
-})
+/**
+ * Button component from Apa UI
+ */
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(({ children, className, ...props }) => (
+    <button
+        className={`text-sm font-semibold shadow-[3px_3px_0_0_#000] border-2 border-black bg-sky-300 h-10 px-4 rounded hover:translate-x-[3px] hover:shadow-none hover:translate-y-[3px] transition-all ${className}`}
+        {...props}
+    >
+        {children}
+    </button>
+))
 
 export default Button
--- components/react/button.jsx
+++ components/react/button.jsx
@@ -1,16 +1,17 @@
 'use client'
 
-import * as React from 'react'
+import { forwardRef } from 'react'
 
-const Button = ({ children, className, ...props }) => {
-    return (
-        <button
-            className={`text-sm font-semibold shadow-[3px_3px_0_0_#000] border-2 border-black bg-sky-300 h-10 px-4 rounded hover:translate-x-[3px] hover:shadow-none hover:translate-y-[3px] transition-all ${className}`}
-            {...props}
-        >
-            {children}
-        </button>
-    )
-}
+/**
+ * Button component from Apa UI
+ */
+const Button = forwardRef(({ children, className, ...props }) => (
+    <button
+        className={`text-sm font-semibold shadow-[3px_3px_0_0_#000] border-2 border-black bg-sky-300 h-10 px-4 rounded hover:translate-x-[3px] hover:shadow-none hover:translate-y-[3px] transition-all ${className}`}
+        {...props}
+    >
+        {children}
+    </button>
+))
 
 export default Button
