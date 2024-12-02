2ed9e545437ddda731e9c50f2337815eda3bd6dd
Zain Adam <zainadam.id@gmail.com>
2024-12-02 10:01:51 +0800

    Add and integrating with `prettier-plugin-tailwindcss`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,12 @@
 import eslint from '@eslint/js'
-import prettierConfig from 'eslint-config-prettier'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
     eslint.configs.recommended,
     tseslint.configs.strict,
     tseslint.configs.stylistic,
-    prettierConfig,
+    eslintPluginPrettierRecommended,
     {
         files: ['**/*.ts', '**/*.tsx'],
         languageOptions: {
