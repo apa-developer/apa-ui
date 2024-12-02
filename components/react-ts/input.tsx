a92549d529874d1b04181dec58dc9c3fbde042d9
Zain Adam <zainadam.id@gmail.com>
2024-12-02 10:04:16 +0800

    fix new rules lint errors from 2ed9e54 changes

--- components/react-ts/input.tsx
+++ components/react-ts/input.tsx
@@ -7,7 +7,7 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<'input'>>(
             <input
                 type={type}
                 className={cn(
-                    'flex h-10 w-full rounded-md border-2 border-black shadow-[2px_2px_0_0_#000] bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:shadow-none focus-visible:translate-y-[2px] focus-visible:translate-x-[2px] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm transition-all',
+                    'bg-background ring-offset-background file:text-foreground placeholder:text-muted-foreground flex h-10 w-full rounded-md border-2 border-black px-3 py-2 text-base shadow-[2px_2px_0_0_#000] transition-all file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:translate-x-[2px] focus-visible:translate-y-[2px] focus-visible:shadow-none focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
                     className
                 )}
                 ref={ref}
