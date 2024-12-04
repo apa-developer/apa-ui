03bd9eef5e0fd9d48997807abdd2d900cae61164
Adam Akbar <zainadam.id@gmail.com>
2024-12-04 15:03:22 +0800

    docs: update documentation and package metadata (#57)
    * docs: remove `module` prop, add `author` and `license` prop
    
    * docs: add comments for semantic-release configuration
    
    * docs(README): update project title, development, releasing, contributing sections, and license

--- release.config.mjs
+++ release.config.mjs
@@ -1,4 +1,9 @@
 /**
+ * Configuration for semantic-release
+ *
+ * @see https://semantic-release.gitbook.io/semantic-release/usage/workflow-configuration
+ * @see https://semantic-release.gitbook.io/semantic-release/usage/configuration
+ *
  * @type {import('semantic-release').GlobalConfig}
  */
 export default {
