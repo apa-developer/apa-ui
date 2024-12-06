aec15bed837d48173e6e32a86aab3d0b7d3bb46c
Adam Akbar <zainadam.id@gmail.com>
2024-12-07 03:54:53 +0800

    fix: lint error (#65)
    * fix: lint error
    
    * ci: add linting step to branches-on-push workflow

--- components/react/button.jsx
+++ components/react/button.jsx
@@ -1,37 +1,36 @@
 'use client'
 
 import { cn } from '@/utils/cn'
-import { cva } from "class-variance-authority"
+import { cva } from 'class-variance-authority'
 
-
-const variants = cva('h-10 flex justify-center items-center rounded border-2 border-black bg-sky-300 text-sm font-semibold shadow-[3px_3px_0_0_#000] transition-all hover:translate-x-[3px] hover:translate-y-[3px] hover:shadow-none', {
-    variants: {
-        iconPosition: {
-            left: 'flex-row',
-            right: 'flex-row-reverse'
+const variants = cva(
+    'h-10 flex justify-center items-center rounded border-2 border-black bg-sky-300 text-sm font-semibold shadow-[3px_3px_0_0_#000] transition-all hover:translate-x-[3px] hover:translate-y-[3px] hover:shadow-none',
+    {
+        variants: {
+            iconPosition: {
+                left: 'flex-row',
+                right: 'flex-row-reverse',
+            },
+            variant: {
+                textOnly: 'justify-center px-4',
+                iconOnly: 'w-10',
+                textAndIcon: 'gap-2 px-2',
+            },
         },
-        variant: {
-            textOnly: 'justify-center px-4', 
-            iconOnly: 'w-10',
-            textAndIcon: 'gap-2 px-2'
-        }
-    },
-    defaultVariants: {
-        iconPosition: 'left',
-        variant: 'textOnly'
+        defaultVariants: {
+            iconPosition: 'left',
+            variant: 'textOnly',
+        },
     }
-})
+)
 
 /**
  * Button component from Apa UI
  */
 const Button = ({ children, className, variant, iconPosition, ...props }) => {
     return (
-        <button
-            className={cn(variants({variant, iconPosition, className}))}
-            {...props}
-        >   
-     {children}
+        <button className={cn(variants({ variant, iconPosition, className }))} {...props}>
+            {children}
         </button>
     )
 }
--- components/react/radio.jsx
+++ components/react/radio.jsx
@@ -1,36 +1,34 @@
-'use client';
+'use client'
 
-import { cn } from "../utils/cn";
+import { cn } from '../utils/cn'
 
 /**
  * RadioGroup component from Apa UI
  */
 export default function RadioGroup({ options, value, onChange }) {
-  return (
-    <div className="flex flex-col space-y-2">
-      {options.map((option) => (
-        <button
-          key={option.value}
-          onClick={() => onChange(option.value)}
-          className="flex items-center"
-          role="radio"
-          aria-checked={value === option.value}
-        >
-          <div
-            className={cn(
-              'mr-2 flex items-center justify-center h-[26px] w-[26px] bg-white border-2 border-black rounded-full transition-all',
-              value === option.value
-                ? 'translate-x-[2px] translate-y-[2px] shadow-none'
-                : 'shadow-[2px_2px_0_0_#000]'
-            )}
-          >
-            {value === option.value && (
-              <div className="h-3 w-3 bg-black rounded-full" />
-            )}
-          </div>
-          <p className="text-black text-sm font-semibold ">{option.label}</p>
-        </button>
-      ))}
-    </div>
-  );
+    return (
+        <div className="flex flex-col space-y-2">
+            {options.map((option) => (
+                <button
+                    key={option.value}
+                    onClick={() => onChange(option.value)}
+                    className="flex items-center"
+                    role="radio"
+                    aria-checked={value === option.value}
+                >
+                    <div
+                        className={cn(
+                            'mr-2 flex h-[26px] w-[26px] items-center justify-center rounded-full border-2 border-black bg-white transition-all',
+                            value === option.value
+                                ? 'translate-x-[2px] translate-y-[2px] shadow-none'
+                                : 'shadow-[2px_2px_0_0_#000]'
+                        )}
+                    >
+                        {value === option.value && <div className="h-3 w-3 rounded-full bg-black" />}
+                    </div>
+                    <p className="text-sm font-semibold text-black">{option.label}</p>
+                </button>
+            ))}
+        </div>
+    )
 }
