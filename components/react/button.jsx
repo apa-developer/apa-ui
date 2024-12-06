b65a7079fd09f80f7cd22111e3868ae7ea54bdf0
Chopper <riskidrian1@gmail.com>
2024-12-06 22:13:24 +0700

    feat: button fix

--- components/react/button.jsx
+++ components/react/button.jsx
@@ -1,21 +1,40 @@
 'use client'
 
+import React from 'react'
 import { cn } from '@/utils/cn'
-import { forwardRef } from 'react'
+import { cva } from "class-variance-authority"
+
+
+const variants = cva('h-10 flex justify-center items-center rounded border-2 border-black bg-sky-300 text-sm font-semibold shadow-[3px_3px_0_0_#000] transition-all hover:translate-x-[3px] hover:translate-y-[3px] hover:shadow-none', {
+    variants: {
+        iconPosition: {
+            left: 'flex-row',
+            right: 'flex-row-reverse'
+        },
+        variant: {
+            textOnly: 'justify-center px-4', 
+            iconOnly: 'w-10',
+            textAndIcon: 'gap-2 px-2'
+        }
+    },
+    defaultVariants: {
+        iconPosition: 'left',
+        variant: 'textOnly'
+    }
+})
 
 /**
  * Button component from Apa UI
  */
-const Button = forwardRef(({ children, className, ...props }) => (
-    <button
-        className={cn(
-            'h-10 rounded border-2 border-black bg-sky-300 px-4 text-sm font-semibold shadow-[3px_3px_0_0_#000] transition-all hover:translate-x-[3px] hover:translate-y-[3px] hover:shadow-none',
-            className
-        )}
-        {...props}
-    >
-        {children}
-    </button>
-))
+const Button = ({ children, className, variant, iconPosition, ...props }) => {
+    return (
+        <button
+            className={cn(variants({variant, iconPosition, className}))}
+            {...props}
+        >   
+     {children}
+        </button>
+    )
+}
 
 export default Button
